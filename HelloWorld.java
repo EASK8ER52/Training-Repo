0817d54ba0861260a4c2f3860f0021ca17ae15ef
EASK8ER52 <eask8er52@gmail.com>
2022-12-05 13:29:18 -0800

    Added Java file to the Git. It prints Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+// This will just print out Hello World
+
+import java.util.*;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        // Local Declarations
+
+        // Processing
+        System.out.println("Hello World!");
+        // Output
+
+    }
+}
