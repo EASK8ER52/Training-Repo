1747de956c137d3f57d054e75c7aee7370d9cab3
EASK8ER52 <eask8er52@gmail.com>
2022-12-08 12:40:10 -0800

    Added a try catch block for ArrayList exceptions.

--- Java-Notes/example/src/main/java/com/example/App.java
+++ Java-Notes/example/src/main/java/com/example/App.java
@@ -28,18 +28,64 @@ public class App
 
         app.get("/hello", ctx -> ctx.result("Hello World!"));
 
-        app.post("/add", ctx -> {
+        app.post("/add", ctx ->
+        {
             String newName = ctx.body();
             names.add(newName);
             ctx.result("Congrats you've added a new name to the database");
             ctx.status(201);
         });
 
-        app.get("/person/{num}", ctx -> {
+        app.get("/person/{num}", ctx ->
+        {
             String numAsString = ctx.pathParam("num");
             int num = Integer.parseInt(numAsString);
             String person = names.get(num);
             ctx.result(person);
+            ctx.status(200);
+        });
+
+        app.get("/protectedGet/{num}", ctx ->
+        {
+            String numAsString = ctx.pathParam("num");
+            int num = Integer.parseInt(numAsString);
+            try
+            {
+                String person = names.get(num);
+                ctx.result(person);
+                ctx.status(200);
+            }
+            catch (IndexOutOfBoundsException e)
+            {
+                ctx.result("No person found in that index position");
+                ctx.status(404);
+            }
+        });
+
+        app.put("/replace/{num}", ctx ->
+        {
+            String numString = ctx.pathParam("num");
+            int num = Integer.parseInt(numString);
+            String replacePerson = ctx.body();
+            names.set(num, replacePerson);
+            ctx.result("Replaced person successfully!");
+            ctx.status(200);
+        });
+
+        app.patch("/update/{num}", ctx ->
+        {
+            String numString = ctx.pathParam("num");
+            int num = Integer.parseInt(numString);
+            String updatedInformation = ctx.body();
+            names.set(num, updatedInformation);
+            ctx.result("Successfully updated the person's information");
+            ctx.status(200);
+        });
+
+        app.delete("/delete", ctx ->
+        {
+            names.clear();
+            ctx.result("All names have been removed from the database");
             ctx.status(200);
         });
 
