dd5c9b7566b01a8c34d4775b9a6aa84dd9386182
EASK8ER52 <eask8er52@gmail.com>
2022-12-08 10:06:16 -0800

    3 Files added after creating the maven project

--- Java-Notes/example/src/main/java/com/example/App.java
+++ Java-Notes/example/src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import io.javalin.Javalin;
+
 /**
- * Hello world!
- *
+ 
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Javalin app = Javalin.create();
+
+        app.get("/hello", ctx -> ctx.result("Hello World!"));
+
+        app.start();
     }
 }
