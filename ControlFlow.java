458317e010cd9dc009dc25fdc936b663349aa387
EASK8ER52 <eask8er52@gmail.com>
2022-12-08 08:43:58 -0800

    Added java Notes Folder

---
+++ ControlFlow.java
@@ -0,0 +1,22 @@
+public class ControlFlow 
+{
+    public static void main(String[] args)
+    {
+        // Local Declarations
+        int visaCard   = 100; // There are $100 in this card
+        int burgerCost = 45;  // Cost of the burger
+        int lowCard    = 40;  // Low card cost
+
+        // Processing
+        if (visaCard >= burgerCost)
+        {
+            System.out.println("Congrats on buying a burger, lord Ramsey is please to serve you");
+        }
+
+        if (lowCard >= burgerCost)
+        {
+            System.out.println("Congrats on buying a burger, lord Ramsey is please to serve you");
+        }
+        // Output
+    }
+}
